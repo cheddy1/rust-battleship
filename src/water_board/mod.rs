dc368ac9a2d860a544dfd34a761fd13fca9c5b95
Ubspy <jack-m@live.com>
2021-09-13 19:40:45 -0500

    Added constant variable for board size

--- src/water_board/mod.rs
+++ src/water_board/mod.rs
@@ -4,10 +4,12 @@ mod data_structures;
 // This tells rust that we want to use everything from the data_structues module in this scope
 use data_structures::*;
 
+const BOARD_SIZE: usize = 8;
+
 pub struct WaterBoard
 {
     // Battleship uses an 8x8 grid, so we're going to make a 2D array of WaterSquares
-    board_matrix: [[ WaterSquare; 8 ]; 8 ]
+    board_matrix: [[ WaterSquare; BOARD_SIZE ]; BOARD_SIZE ]
 }
 
 impl WaterBoard
@@ -23,9 +25,9 @@ impl WaterBoard
         print!("["); // Start the array in the print
 
         // Iterate through the matrix to print it
-        for i in 0..8
+        for i in 0..BOARD_SIZE
         {
-            for j in 0..8
+            for j in 0..BOARD_SIZE
             {
                 // Match statement to show if the square is either empty or has been hit
                 let to_print = match self.board_matrix[i][j]
@@ -43,7 +45,7 @@ impl WaterBoard
                 // Print the value
                 print!(" {}", to_print);
 
-                if i != 7 || j != 7
+                if i != (BOARD_SIZE - 1) || j != (BOARD_SIZE - 1)
                 {
                     // Add a comma if it's not the very last value
                     print!(", ");
@@ -51,7 +53,7 @@ impl WaterBoard
             }
 
             // Print a new line as long as it's not the last line
-            if i != 7
+            if i != (BOARD_SIZE - 1) 
             {
                 print!("\n");
             }
