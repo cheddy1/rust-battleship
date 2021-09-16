edbdd317cadb71ba187a3669d7b99dbcfba4ae70
MorphewPrime <jackmorphew101@gmail.com>
2021-09-15 21:03:52 -0500

    initial GUI commit with working buttons and callbacks

--- src/battleship_game/mod.rs
+++ src/battleship_game/mod.rs
@@ -7,8 +7,17 @@ mod ship;
 use crate::battleship_game::player::*;
 use crate::battleship_game::data_structures::*;
 
+// Import FLTK modules for GUI
+use fltk::{app, button::Button, frame::Frame, enums::FrameType, enums::Color, prelude::*, window::Window};
+
+
 const BOARD_WIDTH: usize = 10;
 const BOARD_HEIGHT: usize = 9;
+const SQUARE_SIZE: i32 = 50;
+
+const P1_BOARD_LEFT_OFFSET: i32 = 50;
+const P2_BOARD_LEFT_OFFSET: i32 = 750;
+const BOARD_TOP_OFFSET: i32 = 100;
 
 pub struct BattleShipGame
 {
@@ -20,6 +29,54 @@ impl BattleShipGame
 {
     pub fn init_game() -> BattleShipGame
     {
+        // I dont know where to put this window creation logic, so it can live in init_game for now.
+        let app = app::App::default();
+        let mut wind = Window::new(100, 100, 1450, 600, "💥 Rust Battleship 💥");
+
+        // Track the buttons with 2d array (broken atm)
+        //let mut p1_button_ary = [[Button::default(); 9]; 10];
+        //let mut p2_button_ary = [[Button::default(); 9]; 10];
+
+
+        for j in 0..BOARD_HEIGHT
+        {
+            for i in 0..BOARD_WIDTH
+            {
+                // Build player 1's board.
+                let mut temp_btn = Button::new(P1_BOARD_LEFT_OFFSET + (i as i32 * SQUARE_SIZE),
+                                               BOARD_TOP_OFFSET + (j as i32 * SQUARE_SIZE), SQUARE_SIZE, SQUARE_SIZE, "B");
+
+                temp_btn.set_frame(FrameType::BorderBox);
+                temp_btn.set_color(Color::from_u32(0x48769C));
+                temp_btn.set_selection_color(Color::from_u32(0x1a2b38));
+
+                // Custom function can replace println in future!
+                temp_btn.set_callback(move |_| println!("Player 1 {},{}", i, j));
+
+                // 2d array currently broken.
+                // p1_buttons[i][j] = temp_btn; // broken
+
+
+                // Build player 2's board.
+                let mut temp_btn2 = Button::new(P2_BOARD_LEFT_OFFSET + (i as i32 * SQUARE_SIZE),
+                                                BOARD_TOP_OFFSET + (j as i32 * SQUARE_SIZE), SQUARE_SIZE, SQUARE_SIZE, "B");
+
+                temp_btn2.set_frame(FrameType::BorderBox);
+                temp_btn2.set_color(Color::from_u32(0x48769C));
+                temp_btn2.set_selection_color(Color::from_u32(0x1a2b38));
+
+                // Custom function can replace println in future!
+                temp_btn2.set_callback(move |_| println!("Player 2 {},{}", i, j));
+
+                // 2d array currently broken.
+                // p2_button_ary[i][j] = temp_btn2; // broken
+            }
+        }
+
+        wind.end();
+        wind.show();
+        app.run().unwrap();
+
         BattleShipGame
         {
             player_one: Player::new_player(Players::PlayerOne),
