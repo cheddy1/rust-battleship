988c03ead6a942db0554d709b38f6d8769b9c4f0
Hayden Myers <hayden.myers@Haydens-MacBook-Air.local>
2021-09-18 16:58:45 -0500

    Added comments to the data_structures file

--- src/battleship_game/data_structures.rs
+++ src/battleship_game/data_structures.rs
@@ -5,18 +5,21 @@
 // We need PartialEq to be able to compre different enum objects without using a match statement
 // just cleans up the code a little bit
 #[derive(Copy, Clone, PartialEq)]
+//Class definition for a water square containing two potential states, Empty and Hit. State will be
+//Empty if it has not been hit by a player and Hit if it has been hit by a player
 pub enum WaterSquare
 {
     Empty,
     Hit,
 }
-
+//Class definition for the players containing two potential states, PlayerOne and PlayerTwo
 pub enum Players
 {
     PlayerOne,
     PlayerTwo,
 }
-
+//Class definition for the fire state containing two potential states, Hit or Miss. State will be Hit
+//if a ship has been hit and Miss if a water square has been hit
 pub enum FireState
 {
     Hit,
